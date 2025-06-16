407612c3a7683b42365b197c667ccc60a820dcd6
Rafa Garcia <rafa@rafagarcia.info>
2025-06-16 18:36:28 +0100

    Input validation in Table constructor

--- lib/table.rb
+++ lib/table.rb
@@ -5,11 +5,13 @@ class Table
   attr_reader :width, :height
 
   def initialize(width, height)
+    raise ArgumentError unless [width, height].all? { |d| d.is_a?(Integer) && d.positive? }
+  
     @width = width
     @height = height
   end
 
   def valid_position?(x_pos, y_pos)
-    x_pos >= 0 && x_pos < @width && y_pos >= 0 && y_pos < @height
+    (0...@width).cover?(x_pos) && (0...@height).cover?(y_pos)
   end
 end
